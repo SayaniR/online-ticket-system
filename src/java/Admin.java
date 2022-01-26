0789ad5a101531e074169ac299e117349899c10e
Prasun <prasunghosh@gmail.com>
2022-01-26 10:31:15 +0530

    committed by Prasun

---
+++ src/java/Admin.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author prasun
+ */
+public class Admin {
+    private int adminId;
+    private String adminPassword;
+    private String adminName;
+    private String adminEmail;
+
+    /**
+     * @return the adminId
+     */
+    public int getAdminId() {
+        return adminId;
+    }
+
+    /**
+     * @param adminId the adminId to set
+     */
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    /**
+     * @return the adminPassword
+     */
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    /**
+     * @param adminPassword the adminPassword to set
+     */
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    /**
+     * @return the adminName
+     */
+    public String getAdminName() {
+        return adminName;
+    }
+
+    /**
+     * @param adminName the adminName to set
+     */
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    /**
+     * @return the adminEmail
+     */
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+
+    /**
+     * @param adminEmail the adminEmail to set
+     */
+    public void setAdminEmail(String adminEmail) {
+        this.adminEmail = adminEmail;
+    }
+
+
+    
+}
